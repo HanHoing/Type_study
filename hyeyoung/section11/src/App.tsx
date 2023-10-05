524845a165116126ca5199300c6d7e2b4a3b9079
HanHoing <hhy6789@gmail.com>
2023-10-05 19:34:07 +0900

    section11

--- hyeyoung/section11/src/App.tsx
+++ hyeyoung/section11/src/App.tsx
@@ -1,29 +1,61 @@
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useReducer } from 'react';
 import './App.css';
 import Editor from './components/Editor';
+import {Todo} from "./types";
+import TodoItem from './components/TodoItem';
 
-interface Todo {
-  id: number;
-  content: string;
+type Action = 
+| {
+  type: "CREATE",
+  data: {
+    id : number;
+    content : string;
+  };
+ }
+| {type: "DELETE"; id: number};
+
+function reducer(state: Todo[], action: Action) {
+  switch(action.type){
+    case 'CREATE':{
+      return [...state, action.data];
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
 }
 
-function App() {
+export const TodoStateContext = React.createContext(null);
 
-  const [todos, setTodos] = useState<Todo[]>([]);
+function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
 
   //컨텐트 추가시마다 id 바뀌도록
   const idRef = useRef(0);
 
+  //추가 기능
   const onClickAdd = (text:string) => {
-    setTodos([
-        ...todos,
-        {
-            id: idRef.current++,
-            content: text,
-        }
-    ]);
+
+    dispatch({
+      type: "CREATE",
+      data:{
+        id: idRef.current++,
+        content: text,
+      }
+    })
+
   }
 
+  //삭제 기능
+  const onClickDelete = (id: number) => {
+    // setTodos(todos.filter((todo=>todo.id !== id))); 
+
+    dispatch({
+      type: "DELETE",
+      id: id,
+    }); 
+  };
+ 
   useEffect(()=>{
     console.log(todos);
   }, [todos]);
@@ -35,6 +67,11 @@ function App() {
       <Editor onClickAdd={onClickAdd}>
         <div>chihld</div>
       </Editor> 
+      <div>
+        {todos.map((todo)=>(
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />  
+        ))}
+      </div>
   </div>);
 }
 
